3990ee71ff093f1b45242b01f65e35c245f6af08
Rajiv <96124202+Rjiv07@users.noreply.github.com>
2025-11-13 11:51:36 +0530

    Add class A with main method to print multiples of 2

---
+++ A.java
@@ -0,0 +1,12 @@
+class A
+  {
+    public static void main(String[] args)
+    {
+      for(int i=0; i<11; i++)
+        {
+          System.out.println("2"+"X"+ i + "=" + 2*i);
+        }
+    }
+  } 
+
+
